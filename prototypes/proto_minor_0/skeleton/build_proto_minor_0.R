8bff8703905ac67b8708da23ad9f6931d270719b
colonelsmoothie <colonelsmoothie@gmail.com>
2014-03-09 21:54:57 -0500

    initialized build script for proto_minor_0

---
+++ prototypes/proto_minor_0/skeleton/build_proto_minor_0.R
@@ -0,0 +1,11 @@
+setwd("./github/trantor-minor")
+
+### Source scripts that contain functions
+### We'll need these to build the tables
+source(./componenets/functions/generate_dates.R)
+source(./generate_names.R)
+
+### Generate policy table
+
+
+### Generate claim table
