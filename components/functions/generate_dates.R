59d7cac479825467f63b703dc19c83fb602f4c6e
colonelsmoothie <colonelsmoothie@gmail.com>
2014-03-21 23:27:12 -0500

    uncommented what I commented last time

--- components/functions/generate_dates.R
+++ components/functions/generate_dates.R
@@ -23,16 +23,16 @@ generate_dates <- function(n, lbound, ubound, idistr="unif", ldistr=static(1)){
   to_date <- as.numeric(as.Date(ubound)) + 1
   # we use + 1 to ensure that policies written on the last date
   # can be generated
-  #if(idistr=="unif"){
+  if(idistr=="unif"){
     incept_dates <- runif(n, from_date, to_date)
-  #}
+  }
   class(incept_dates) <- 'Date'
-  #if(is.numeric(ldistr) & length(ldistr)==1){
+  if(is.numeric(ldistr) & length(ldistr)==1){
     exp_dates <- incept_dates
     # checks to see if incept year is a leap year, if so, subtract a day as we will not be able to add a year to 02-29
     exp_dates[leap_year(exp_dates)] <- exp_dates[leap_year(exp_dates)] - 1
     exp_dates <- exp_dates + years(1)
-  #}
+  }
   return(data.frame(Incept_Date=incept_dates,Exp_Date=exp_dates))
 }
 
