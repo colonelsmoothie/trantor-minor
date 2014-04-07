b37d54bc7f51d839405c534d0cb9ad871b10f840
colonelsmoothie <colonelsmoothie@gmail.com>
2014-04-06 22:41:15 -0500

    updated prem transaction function

---
+++ components/functions/gen_prem_transact.R
@@ -0,0 +1,9 @@
+### Takes Policy data frame as input and generates premium transactions using the policy dates
+### Arguments:
+### Policy - the policy data frame
+### interval - payment interval (monthly, weekly, etc.)
+
+gen_prem_transact <- function(Policy,interval){
+  polcount <- nrow(Policy)
+  
+}
