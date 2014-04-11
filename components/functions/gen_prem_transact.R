b649208fc816e20628437ca3efed82ac2f775227
colonelsmoothie <colonelsmoothie@gmail.com>
2014-04-10 23:30:59 -0500

    updated prem transact function

--- components/functions/gen_prem_transact.R
+++ components/functions/gen_prem_transact.R
@@ -8,6 +8,7 @@ gen_prem_transact <- function(Policy,n.interval=12){
   for(i in 1:polcount){
     pollen <- as.numeric(Policy[i,"Exp_Date"] - Policy[i,"Incept_Date"])
     pay.period <- pollen/n.interval
+    pay.dates <- seq(as.numeric(Policy[i,"Incept_Date"],as.numeric(Policy[i,"Exp_Date"]),n.interval))
   }
   
 }
