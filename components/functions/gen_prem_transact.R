23b57e7154b79f49f8ccb54b1861c7696423d80f
colonelsmoothie <colonelsmoothie@gmail.com>
2014-04-11 23:14:55 -0500

    calculates mthly payment

--- components/functions/gen_prem_transact.R
+++ components/functions/gen_prem_transact.R
@@ -9,6 +9,7 @@ gen_prem_transact <- function(Policy,n.interval=12){
     pollen <- as.numeric(Policy[i,"Exp_Date"] - Policy[i,"Incept_Date"])
     pay.period <- pollen/n.interval
     pay.dates <- seq(as.numeric(Policy[i,"Incept_Date"],as.numeric(Policy[i,"Exp_Date"]),n.interval))
+    payment.amt <- Policy[i,"GrossWrittenPremium"] / n.interval
   }
   
 }
