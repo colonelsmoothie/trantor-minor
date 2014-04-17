d2d48d3ca04ff62adfb2b4543a8ab35d7a877937
colonelsmoothie <colonelsmoothie@gmail.com>
2014-04-16 22:18:35 -0500

    updated prem transact

--- components/functions/gen_prem_transact.R
+++ components/functions/gen_prem_transact.R
@@ -7,6 +7,8 @@
 ### initialize variables for testing - delete later
 
 
+### might want to check out if GWP is necessary in the other table, maybe make a distinction between OGWP and GWP actually realised
+
 gen_prem_transact <- function(Policy,n.interval=12){
   polcount <- nrow(Policy)
   for(i in 1:polcount){
