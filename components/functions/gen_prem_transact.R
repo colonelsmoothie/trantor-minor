d03d73faf81caeea75429d315e727bd9b167ed82
colonelsmoothie <colonelsmoothie@gmail.com>
2014-05-13 22:18:18 -0500

    added transact id

--- components/functions/gen_prem_transact.R
+++ components/functions/gen_prem_transact.R
@@ -26,9 +26,48 @@ gen_prem_transact <- function(Policy,n.interval=12){
 
 
 ### Unencapsulated code for testing
+
+setwd("./github/trantor-minor")
+
+### Import dataset
+### Import names from name files
+firstnames <- as.matrix(read.csv("./components/names/CSV_Database_of_First_Names.csv", header=TRUE))
+lastnames <-as.matrix(read.csv("./components/names/CSV_Database_of_Last_Names.csv", header=TRUE))
+
+
+### Source scripts that contain functions
+### We'll need these to build the tables
+source("./components/functions/generate_dates.R")
+source("./components/functions/generate_names.R")
+source("./components/functions/generate_claims.R")
+
+
+### Specify db parameters here
+polcount <- 5
+claimsize <- 5000
+freq <- .08
+prem <- claimsize * freq
+
+Policy_ID <- 1:polcount
+set.seed(52)
+Names <- generate_names(polcount,firstnames,lastnames)
+### check names
+head(Names)
+
+set.seed(97)
+Dates <- generate_dates(polcount,"2000-01-01","2005-01-01")
+head(Dates)
+
+GrossWrittenPremium <- rep(prem,polcount)
+
+Policy <- cbind(Policy_ID,Names,Dates,GrossWrittenPremium)
+head(Policy)
+
+#### test table
 Policy
 i <- 1
 n.interval = 12
+curr.transact <- 1
 pollen <- as.numeric(Policy[i,"Exp_Date"] - Policy[i,"Incept_Date"])
 pollen
 ### calculate length of each interval
@@ -46,6 +85,7 @@ prem.pmts
 ### check to see if sum of payments equals GWP
 sum(prem.pmts) == Policy[i,"GrossWrittenPremium"]
 ### transact id | policy id | transaction date | payment amount
-prem.transacts <- cbind(i,pay.dates,payment.amt)
+transact.ids <- seq(curr.transact, curr.transact + n.interval - 1,1)
+prem.transacts <- data.frame(transact.ids,i,pay.dates,prem.pmts)
 prem.transacts
-##
+### will need to change transaction order and add a tie breaker, probably by pol id
